b58d9879cc7a11c80876af8d6b739a5d8a91adb2
Caren Chang <calren24@gmail.com>
2017-06-09 20:55:15 -0700

    add gif

--- app/src/main/java/com/example/caren/animations/FlingAnimationActivity.java
+++ app/src/main/java/com/example/caren/animations/FlingAnimationActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.animation.DynamicAnimation;
 import android.support.animation.FlingAnimation;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.SeekBar;
@@ -16,9 +15,9 @@ public class FlingAnimationActivity extends AppCompatActivity {
     TextView startVText;
     TextView frictionText;
     FlingAnimation mFlingAnimation;
-    
+
     View ghostView;
-    float ghostViewYStart;
+    float viewStartXPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +25,15 @@ public class FlingAnimationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fling_animation);
 
         ghostView = findViewById(R.id.ghost);
-        ghostViewYStart = ghostView.getTranslationY();
+        viewStartXPos = ghostView.getTranslationX();
         mFlingAnimation = new FlingAnimation(ghostView, DynamicAnimation.TRANSLATION_X);
+        // moves the view on the x-axis
+        findViewById(R.id.animateButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startFlingAnimation();
+            }
+        });
 
         startVelocitySeekbar = findViewById(R.id.startVelocity);
         frictionSeekbar = findViewById(R.id.friction);
@@ -40,7 +46,7 @@ public class FlingAnimationActivity extends AppCompatActivity {
                 startVText.setText(
                         "start velocity value: " +
                                 (startVelocitySeekbar.getProgress() == 0 ? 1 :
-                                        startVelocitySeekbar.getProgress()));
+                                        startVelocitySeekbar.getProgress() * 50));
             }
 
             @Override
@@ -56,8 +62,9 @@ public class FlingAnimationActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 frictionText.setText(
-                        "friction value: " + (frictionSeekbar.getProgress() == 0 ? 1 :
-                                frictionSeekbar.getProgress() / 50f));
+                        "friction value: " +
+                                (frictionSeekbar.getProgress() == 0 ? 0.01f :
+                                        frictionSeekbar.getProgress() / 100f));
             }
 
             @Override
@@ -66,30 +73,30 @@ public class FlingAnimationActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                startFlingAnimation();
             }
         });
     }
 
     private void startFlingAnimation() {
+        // usually between 200 to 20,000, negative if going right to left
         float startVelocity = startVelocitySeekbar.getProgress() == 0 ? 1 :
-                startVelocitySeekbar.getProgress() *
-                        50; // 200 to 20,000, negative if going// right to left
+                startVelocitySeekbar.getProgress() * 50;
 
+        // derive the velocity in pixels per second
         float pixelPerSecond = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                startVelocity,
-                getResources().getDisplayMetrics());
+                startVelocity, getResources().getDisplayMetrics());
 
         float friction =
-                frictionSeekbar.getProgress() == 0 ? 0.01f : frictionSeekbar.getProgress() / 50f;
+                frictionSeekbar.getProgress() == 0 ? 0.01f : frictionSeekbar.getProgress() / 100f;
+
         mFlingAnimation.setStartVelocity(pixelPerSecond)
                 .setFriction(friction)
                 .addEndListener(new DynamicAnimation.OnAnimationEndListener() {
                     @Override
                     public void onAnimationEnd(DynamicAnimation dynamicAnimation, boolean b,
                             float v, float v1) {
-                        Log.i("Caren", "Resetting ghost view: " + ghostViewYStart);
-                        ghostView.setY(ghostViewYStart);
+                        // reset view position
+                        ghostView.setTranslationX(viewStartXPos);
                     }
                 })
                 .start();
